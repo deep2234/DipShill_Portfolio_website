838a050157550d191a7b475bb2e58bec974cbf26
deep2234 <dipshill007@gmail.com>
2025-07-24 21:26:38 +0530

    initial

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const counter = document.querySelector("#counter-number");
+
+async function updateCounter() {
+    let response = await fetch("");
+    let data = await response.json();
+    counter.innerHTML = `views: ${data}`;
+}
+
+updateCounter();
